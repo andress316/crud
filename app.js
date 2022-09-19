a8a094a7cf35a9142c116f8877c79edd456a3db3
Andres Reyes <andresreyes@MacBook-Pro-de-Andres.local>
2022-09-19 15:28:57 -0300

    Se agregó la función de editar

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 
-// Esta función ejecutará las variables globales de la aplicación
-function Submit(){
+var fila = null;
+
+
+
+// Esta función mostrará los datos en la tabla de eregistro
+function Submit() {
     //PASO 1 - Esta variable hace la lectura de datos
     var datosIngresados = recuperaDatos();
     // PASO 2 - Esta variable almacena los datos leidos en el local storage
     var lecturaDeDatos = lecturaDeLocalStorage(datosIngresados);
-    // PASO 3 - Se ejecuta la función de insertar datos a la tabla de registro
+   
+    if(fila == null){
     insertar(lecturaDeDatos);
+    }
+    else {
+        actualizar();
+    }
+
+
 }
 
 
 //PASO 1 - Acá ingresaré los datos del form en un Arreglo
-function recuperaDatos (){
+function recuperaDatos() {
     var nombre = document.getElementById("nombre").value;
     var cargo = document.getElementById("cargo").value;
     var exp = document.getElementById("exp").value;
 
-    var arr= [nombre, cargo, exp];
+    var arr = [nombre, cargo, exp];
     return arr;
 }
 
 
 //PASO 2 - Lectura de datos almacenados localmente
-function lecturaDeLocalStorage(datosIngresados){
+function lecturaDeLocalStorage(datosIngresados) {
 
     // Acá alamaceno los datos en el local storage
     var n = localStorage.setItem("nombre", datosIngresados[0]);
@@ -35,12 +46,12 @@ function lecturaDeLocalStorage(datosIngresados){
     var e1 = localStorage.getItem("experiencia", e);
 
     // Acá almaceno los datos leidos en un array
-    var arr = [n1 , c1 , e1];
+    var arr = [n1, c1, e1];
     return arr;
 }
 
 //PASO 3 - Acá insertamos los datos a la tabla de registro
-function insertar(lecturaDeDatos){
+function insertar(lecturaDeDatos) {
     var fila = registro.insertRow();
     var col1 = fila.insertCell(0);
     var col2 = fila.insertCell(1);
@@ -50,7 +61,27 @@ function insertar(lecturaDeDatos){
     col1.innerHTML = lecturaDeDatos[0];
     col2.innerHTML = lecturaDeDatos[1];
     col3.innerHTML = lecturaDeDatos[2];
-    col4.innerHTML = `<button>Editar</button>
-    <button>Eliminar</button>`;
-  
+    col4.innerHTML = `<button onclick = edit(this)>Editar</button>
+                      <button onclick = eliminar(this)>Eliminar</button>`;
 }
+
+
+/*_________________EDITAR DATOS_____________________________ */
+
+function edit(dat){
+    fila = dat.parentElement.parentElement;
+    document.getElementById("nombre").value = fila.cells[0].innerHTML;
+    document.getElementById("cargo").value = fila.cells[1].innerHTML;
+    document.getElementById("exp").value = fila.cells[2].innerHTML;
+}
+
+/*_________________ACTUALIZAR DATOS_____________________________ */
+
+function actualizar(){
+    fila.cells[0].innerHTML = document.getElementById("nombre").value;
+    fila.cells[1].innerHTML = document.getElementById("cargo").value;
+    fila.cells[2].innerHTML = document.getElementById("exp").value;
+    fila = null;
+}
+
+
